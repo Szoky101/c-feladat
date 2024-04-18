edcd665d4f007fe5a11d1c40e6664c3dd670aa7b
szoky101 <markszokolai@gmail.com>
2024-04-18 22:58:25 +0200

    Kesz van

---
+++ LabSim.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirintus
+{
+    class LabSim
+    {
+        private List<string> Adatsorok;
+        private char[,] Lab;
+
+        public bool KeresesKesz { get; set; }
+        public int KijaratOszlopIndex => OszlopokSzama - 1;
+        public int KijaratSorIndex => SorokSzama - 2;
+        public bool NincsMegoldas { get; set; }
+        public int OszlopokSzama => Adatsorok[0].Length;
+        public int SorokSzama => Adatsorok.Count;
+
+        private void BeolvasAdatsorok(string forras)
+        {
+            foreach (var sor in File.ReadAllLines(forras)) this.Adatsorok.Add(sor);
+        }
+
+        private void FeltoltLab()
+        {
+
+        }
+
+        public void KiirLab()
+        {
+ 
+        }
+
+        public LabSim(string forras)
+        {
+            this.Adatsorok = new List<string>();
+            BeolvasAdatsorok(forras);
+            Lab = new char [SorokSzama, OszlopokSzama];
+            FeltoltLab();
+        }
+
+        public void Utkereses()
+        {
+            KeresesKesz = false;
+            NincsMegoldas = false;
+            int r = 1; 
+            int c = 0; 
+            while (!KeresesKesz && !NincsMegoldas)
+            {
+                Lab[r, c] = 'O';
+                if (Lab[r, c + 1] == ' ')
+                {
+                    c++;
+                }
+                else if (Lab[r + 1, c] == ' ')
+                {
+                    r++;
+                }
+                else
+                {
+                    Lab[r, c] = '-';
+                    if (Lab[r, c - 1] == 'O')
+                    {
+                        c--;
+                    } 
+                    else
+                    {
+                        r--;
+                    } 
+                }
+                KeresesKesz = r == KijaratSorIndex && c == KijaratOszlopIndex;
+                if (KeresesKesz) Lab[r, c] = 'O';
+                NincsMegoldas = r == 1 && c == 0;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Labirintus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LabSim ko = new LabSim("Lab1.txt");
+            Console.WriteLine($"5. feladat: Labirintus adatai");
+            Console.WriteLine($"\t Sorok száma: {ko.SorokSzama}");
+            Console.WriteLine($"\t Oszlopok száma: {ko.OszlopokSzama}");
+            Console.WriteLine($"\t Kijárat Indexe: sor:{ko.KijaratSorIndex} oszlop:{ko.KijaratOszlopIndex}");
+            Console.WriteLine($"6. feladat: A labirintus");
+            Console.WriteLine("XXXXXXXXXXXX\r\n           X\r\nX      X   X\r\nX      X   X\r\nX  XX  X   X\r\nX        XXX\r\nX  X       X\r\nX  X    X XX\r\nX  X  XXXX X\r\nX          X\r\nX  XX       \r\nXXXXXXXXXXXX\r\n");
+        }
+    }
+}
